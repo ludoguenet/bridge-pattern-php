63b5350498dfa788f73b88410efe5da13a256631
ludoguenet <ludovicguenet@gmx.com>
2024-07-12 16:10:48 +0200

    fix: h1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Report\HtmlReport;
+use App\Report\PdfReport;
 use App\Report\SalesHTMLReport;
 use App\Report\SalesPdfReport;
+use App\Report\SalesReport;
 use App\Report\UsersHTMLReport;
 use App\Report\UsersPdfReport;
 use Illuminate\Support\Facades\Route;
@@ -11,8 +14,8 @@ Route::get('/', function () {
 });
 
 Route::get('generate-reports', function () {
-//    return (new UsersHTMLReport)('<h1>Users Report</h2>');
-//    return (new SalesHTMLReport)('<h1>Sales Report</h2>');
-//    return (new UsersPdfReport)('<h1>Users Report</h2>');
-//    return (new SalesPdfReport())('<h1>Sales Report</h2>');
+//    return (new UsersHTMLReport)('<h1>Users Report</h1>');
+//    return (new SalesHTMLReport)('<h1>Sales Report</h1>');
+//    return (new UsersPdfReport)('<h1>Users Report</h1>');
+//    return (new SalesPdfReport())('<h1>Sales Report</h1>');
 });
